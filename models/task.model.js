273a66d4335d5e95d1091dfc20b15393a20cbb38
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-04-19 21:28:32 +0200

    add description

--- models/task.model.js
+++ models/task.model.js
@@ -2,44 +2,49 @@ const { Schema, model, Types } = require('mongoose');
 const Category = require('./category.model');
 const User = require('./user.model');
 
+// models/task.model.js
 const taskSchema = new Schema(
     {
-        name : {
-            type : String,
-            required : true,
-            trim : true
+        name: {
+            type: String,
+            required: true,
+            trim: true
         },
-        isDone : {
-            type : Boolean,
-            required : true,
-            default : false
+        description: {
+            type: String,
+            trim: true,
+            default: ""
+            // Pas required, c'est optionnel
         },
-        before : {
-            type : String
-            /* ne pas mettre required = required false */
+        isDone: {
+            type: Boolean,
+            required: true,
+            default: false
         },
-        categoryId : {
-            type : Types.ObjectId, 
-            /* Pour préciser que c'est un type ObjectId on doit importer Types, qui est un objet qui contient tous les types bizaroïdes */
-            ref : Category,
-            /* Pour créer une référence vers le model Category  */
-            required : true
+        before: {
+            type: String
         },
-        fromUserId : {
-            type : Types.ObjectId,
-            ref : User,
-            required : true
+        categoryId: {
+            type: Types.ObjectId,
+            ref: Category,
+            required: true
         },
-        toUserId : {
-            type : Types.ObjectId,
-            ref : User,
-            required : true,
+        fromUserId: {
+            type: Types.ObjectId,
+            ref: User,
+            required: true
+        },
+        toUserId: {
+            type: Types.ObjectId,
+            ref: User,
+            required: true,
         }
     },
     {
-        collection : 'Task',
-        timestamps : true
-    });
+        collection: 'Task',
+        timestamps: true
+    }
+);
 
 const Task = model('Task', taskSchema);
 
